2a1053c4dc9616d9ee745c7100d0c40936da274d
proraj9088 <rp8735685@gmail.com>
2021-03-25 21:16:02 +0530

    UI creted
    Signed-off-by: proraj9088 <rp8735685@gmail.com>

---
+++ src/data.js
@@ -0,0 +1,164 @@
+// API key: 3ca5df7
+
+export const data = [
+    {
+        Title: 'The Avengers',
+        Year: '2012',
+        Rate: 'PG-13',
+        Released: '04 May 2012',
+        Runtime: '143 min',
+        Genre: 'Action, Adventure, Sci-Fi',
+        Director: 'Joss Whedon',
+        Writer: 'Joss Whedon (screenplay), Zak Penn (story), Joss Whedon (story)',
+        Actors: 'Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth',
+        Plot: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+        Language: 'English, Russian, Hindi',
+        Country: 'USA',
+        Awards: 'Nominated for 1 Oscar. Another 38 wins & 79 nominations',
+        Poster: "https://images-na.ssl-images-amazon.com/images/I/71niXI3lxlL._AC_SY679_.jpg",
+        Ratings: [
+            {
+                Source: 'Internet Movie Database',
+                Value: '8.0/10'
+            },
+            {
+                Source: 'Rotten Tomatoes',
+                Value: '91%'
+            },
+            {
+                Source: 'Metacritic',
+                value: '69/100'
+            }
+        ],
+        Metascore: '69',
+        imdbRating: '8.0',
+        imdbVotes: '1,216,895',
+        imdbID: 'tt0848228',
+        Type: 'movie',
+        DVD: '25 Sep 2017',
+        BoxOffice: '$623,279,547',
+        Production: 'Walt Disney Pictures',
+        Website: 'N/A',
+        Response: 'True'
+    },
+    {
+        Title: 'The Dark Knight',
+        Year: '2008',
+        Rate: 'PG-13',
+        Released: '08 May 2008',
+        Runtime: '152 min',
+        Genre: 'Action, Crime, Thriller',
+        Director: 'Christopher Nolan',
+        Writer: 'Jonathan Nolan (screenplay), christopher Nolan (screenplay), christopher Nolan (story)',
+        Actors: 'Christian Bale, Health Ledge, Aaron Eckhart, Michael Caine',
+        Plot: "At the end of The Dark Knight Rises, with the help of Gotham City's remaining police force, Batman was able to get control of the city back from Bane and finally defeated .",
+        Language: 'English, Russian, Hindi',
+        Country: 'USA',
+        Awards: 'Nominated for 1 Oscar. Another 38 wins & 79 nominations',
+        Poster: "https://contentserver.com.au/assets/598411_p173378_p_v8_au.jpg",
+        Ratings: [
+            {
+                Source: 'Internet Movie Database',
+                Value: '9.0/10'
+            },
+            {
+                Source: 'Rotten Tomatoes',
+                Value: '95%'
+            },
+            {
+                Source: 'Metacritic',
+                value: '67/100'
+            }
+        ],
+        Metascore: '69',
+        imdbRating: '9.0',
+        imdbVotes: '1,216,895',
+        imdbID: 'tt0848228',
+        Type: 'movie',
+        DVD: '25 Sep 2008',
+        BoxOffice: '$623,279,547',
+        Production: 'Walt Disney Pictures',
+        Website: 'N/A',
+        Response: 'True'
+    },
+    {
+        Title: 'Gravity',
+        Year: '2013',
+        Rate: 'PG-13',
+        Released: '04 May 2012',
+        Runtime: '143 min',
+        Genre: 'Action, Adventure, Sci-Fi',
+        Director: 'Joss Whedon',
+        Writer: 'Joss Whedon (screenplay), Zak Penn (story), Joss Whedon (story)',
+        Actors: 'Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth',
+        Plot: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+        Language: 'English, Russian, Hindi',
+        Country: 'USA',
+        Awards: 'Nominated for 1 Oscar. Another 38 wins & 79 nominations',
+        Poster: "https://i.pinimg.com/originals/30/a2/9b/30a29b320fe21710496e3028f432d75b.jpg",
+        Ratings: [
+            {
+                Source: 'Internet Movie Database',
+                Value: '7.0/10'
+            },
+            {
+                Source: 'Rotten Tomatoes',
+                Value: '91%'
+            },
+            {
+                Source: 'Metacritic',
+                value: '69/100'
+            }
+        ],
+        Metascore: '69',
+        imdbRating: '7.0',
+        imdbVotes: '1,216,895',
+        imdbID: 'tt0848228',
+        Type: 'movie',
+        DVD: '25 Sep 2017',
+        BoxOffice: '$623,279,547',
+        Production: 'Walt Disney Pictures',
+        Website: 'N/A',
+        Response: 'True'
+    },
+    {
+        Title: 'Iron Man',
+        Year: '2008',
+        Rate: 'PG-13',
+        Released: '05 June 2008',
+        Runtime: '143 min',
+        Genre: 'Action, Adventure, Sci-Fi',
+        Director: 'Joss Whedon',
+        Writer: 'Joss Whedon (screenplay), Zak Penn (story), Joss Whedon (story)',
+        Actors: 'Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth',
+        Plot: "A billionaire industrialist and genius inventor, Tony Stark (Robert Downey Jr.), is conducting weapons tests overseas, but terrorists kidnap him to force him to build a devastating weapon.",
+        Language: 'English, Russian, Hindi',
+        Country: 'USA',
+        Awards: 'Nominated for 1 Oscar. Another 38 wins & 79 nominations',
+        Poster: "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_.jpg",
+        Ratings: [
+            {
+                Source: 'Internet Movie Database',
+                Value: '7.9/10'
+            },
+            {
+                Source: 'Rotten Tomatoes',
+                Value: '91%'
+            },
+            {
+                Source: 'Metacritic',
+                value: '69/100'
+            }
+        ],
+        Metascore: '69',
+        imdbRating: '7.9',
+        imdbVotes: '1,216,895',
+        imdbID: 'tt0848228',
+        Type: 'movie',
+        DVD: '25 Sep 2017',
+        BoxOffice: '$623,279,547',
+        Production: 'Walt Disney Pictures',
+        Website: 'N/A',
+        Response: 'True'
+    }
+]
